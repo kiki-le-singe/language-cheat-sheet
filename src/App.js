f89a660cd857661a6fdcc94eeb5e4368d7003606
kiki le singe <aalbertini95@gmail.com>
2017-05-07 19:28:58 +0200

    feat(App): implement StackNavigator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import { StackNavigator } from 'react-navigation'
+import { StackNavigator, TabNavigator } from 'react-navigation'
 
-import HomeScreen from './Screens/HomeScreen'
+import IrregularVerbDetailScreen from './Screens/IrregularVerbDetailScreen'
+import SheetCheatScreen from './Screens/SheetCheatScreen'
 import IrregularVerbsScreen from './Screens/IrregularVerbsScreen'
 
-export default StackNavigator({
-  Home: { screen: HomeScreen },
-  IrregularVerbs: { screen: IrregularVerbsScreen },
-})
+import colors from './styles/colors'
+
+const { blue, white } = colors
+const LanguageCheatSheetTabNavigator = TabNavigator(
+  {
+    SheetCheat: { screen: SheetCheatScreen },
+    IrregularVerbs: { screen: IrregularVerbsScreen },
+  },
+  {
+    swipeEnabled: true,
+    animationEnabled: true,
+    lazy: true,
+    tabBarOptions: {
+      activeTintColor: white,
+      activeBackgroundColor: blue,
+      labelStyle: {
+        fontSize: 12,
+        fontWeight: 'bold',
+      },
+
+      // Android
+      indicatorStyle: {
+        backgroundColor: white,
+      }
+    },
+  }
+)
+
+// Use default title for all tab screens
+LanguageCheatSheetTabNavigator.navigationOptions = {
+  title: 'Language Cheat Sheet',
+}
+
+export default StackNavigator(
+  {
+    Home: { screen: LanguageCheatSheetTabNavigator },
+    IrregularVerbDetail: { screen: IrregularVerbDetailScreen },
+  },
+)
