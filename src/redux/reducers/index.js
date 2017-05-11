b1cd3a158c0e845a35b08348cccedc3ec81109b7
kiki le singe <aalbertini95@gmail.com>
2017-05-12 00:11:26 +0200

    feat(reducers): add irregularVerbs reducer

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import irregularVerbs from './irregularVerbs'
+
+const rootReducer = combineReducers({
+  irregularVerbs,
+})
+
+export default rootReducer
