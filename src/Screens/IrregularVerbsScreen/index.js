e95a527d97ca2953526e69828a5e2fc923be9bc1
kiki le singe <aalbertini95@gmail.com>
2017-05-12 00:13:51 +0200

    feat(IrregularVerbsScreen): fetch and display irregular verbs from the redux store

--- src/Screens/IrregularVerbsScreen/index.js
+++ src/Screens/IrregularVerbsScreen/index.js
@@ -1,9 +1,13 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, PureComponent } from 'react'
 import {
   View,
   Text,
   Button,
 } from 'react-native'
+import { connect } from 'react-redux'
+
+import { fetch } from '../../redux/actions/IrregularVerbsActions'
+import IrregularVerbs from '../../components/IrregularVerbs'
 
 const Foo = () => (
   <View>
@@ -17,7 +21,12 @@ const Search = () => (
   </View>
 )
 
-export default class IrregularVerbsScreen extends React.PureComponent {
+const actions = { fetch }
+const mapStateToProps = state => ({
+  irregularVerbs: state.irregularVerbs,
+})
+
+export class IrregularVerbsScreen extends PureComponent {
   static navigationOptions = ({ navigation }) => {
     const { state, setParams } = navigation
     const isSearch = state.params && state.params.mode === 'search'
@@ -38,14 +47,28 @@ export default class IrregularVerbsScreen extends React.PureComponent {
 
   static propTypes = {
     navigation: PropTypes.object.isRequired,
+    fetch: PropTypes.func.isRequired,
+    irregularVerbs: PropTypes.array.isRequired,
+  }
+
+  componentWillMount() {
+    this.props.fetch()
   }
 
   render() {
-    const { navigate } = this.props.navigation
+    const { navigation, irregularVerbs } = this.props
+    const { navigate } = navigation
+
+    if (!irregularVerbs.length) {
+      return null
+    }
 
     return (
       <View>
         <Text>List of Irregular Verbs</Text>
+
+        <IrregularVerbs data={irregularVerbs} />
+
         <Button
           onPress={() => navigate('IrregularVerbDetail', { verb: 'Can' })}
           title="Go to Irregular Verbs Detail Screen"
@@ -54,3 +77,5 @@ export default class IrregularVerbsScreen extends React.PureComponent {
     )
   }
 }
+
+export default connect(mapStateToProps, actions)(IrregularVerbsScreen)
